44adc00c07a7a5a93d17784f380d09b45e2420c0
niolmo <71871013+niolmo@users.noreply.github.com>
2023-04-27 17:06:22 +0500

    shell Start

--- NewPap/news/models.py
+++ NewPap/news/models.py
@@ -1,10 +1,24 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Sum
 
 
 class Author(models.Model):
     author = models.OneToOneField(User, on_delete=models.CASCADE)
     authorRunk = models.IntegerField(default=0)
+
+    # МЕТОДЫ
+
+    def update_rating(self):
+        poRunk = self.posts.all().aggregate(postRating=Sum('postRunk'))
+        p_R = 0
+        p_R += poRunk.get(postRating=Sum('postRunk'))
+
+        comRunk = self.comments.all().aggregate(commRating=Sum('commRunk'))
+        c_R = 0
+        c_R += comRunk.get('commRating')
+
+        self.authorRunk = p_R * 3 + c_R
 
 
 class Category(models.Model):
@@ -29,6 +43,18 @@ class Post(models.Model):
     postText = models.TextField()
     postRunk = models.IntegerField(default=0)
 
+    # МЕТОДЫ
+    def like(self):
+        self.postRunk += 1
+        self.save()
+
+    def dislike(self):
+        self.postRunk -= 1
+        self.save()
+
+    def preview(self):
+        return self.postText[0: 124] + '...'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -42,3 +68,10 @@ class Comment(models.Model):
     commCreaTime = models.DateTimeField(auto_now_add=True)
     commRunk = models.IntegerField(default=0)
 
+    def like(self):
+        self.commRunk += 1
+        self.save()
+
+    def dislike(self):
+        self.commRunk -= 1
+        self.save()
