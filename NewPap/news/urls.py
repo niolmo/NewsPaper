3610386a8b4355aa0f7ddd9a8d7a6bf8d1603220
niolmo <71871013+niolmo@users.noreply.github.com>
2023-05-14 10:56:07 +0500

    inner HTML 2

--- NewPap/news/urls.py
+++ NewPap/news/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import NewsList
+from .views import NewsList, NewsDetail
 
 urlpatterns = [
     path('', NewsList.as_view()),
-
+    path('<int:pk>', NewsDetail.as_view()),
 ]
--- NewPap/news/views.py
+++ NewPap/news/views.py
@@ -1,9 +1,14 @@
 # from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
 class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+class NewsDetail(DetailView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
