8f5ba5e61e63a78a1ce7cb4f367217fe6bdbcb15
niolmo <71871013+niolmo@users.noreply.github.com>
2023-05-17 11:30:45 +0500

    inner HTML 2

--- NewPap/news/views.py
+++ NewPap/news/views.py
@@ -1,4 +1,6 @@
 # from django.shortcuts import render
+import datetime
+
 from django.views.generic import ListView, DetailView
 from .models import Post
 
@@ -8,7 +10,15 @@ class NewsList(ListView):
     template_name = 'news.html'
     context_object_name = 'news'
 
+
 class NewsDetail(DetailView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
+
+
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['time_now'] = datetime.UTC
+    context['value'] = None
+    return context
