31c9552d8579fcf6417a8dae35777af918038457
niolmo <71871013+niolmo@users.noreply.github.com>
2023-05-24 14:13:09 +0500

    final

--- NewPap/news/templatetags/custom_filters.py
+++ NewPap/news/templatetags/custom_filters.py
@@ -12,3 +12,16 @@ def multiply(value, arg):
         raise ValueError(
             f'Нельзя умножить {type(value)} на {type(arg)}')  # в случае, если кто-то неправильно воспользовался
         # нашим тегом, выводим ошибку
+
+
+badWords = ['Узнайте', 'словесными', 'Lorem', 'Панграмма', ]
+
+
+@register.filter(name='Censor')
+def censor(value):
+    text = value.split()
+    for i in text:
+        if isinstance(i, str) and i in badWords:
+            return value.replace(i, '******')
+        else:
+            return value
