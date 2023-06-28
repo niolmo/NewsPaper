d2423b9ee2be3ef590483806fb03ecdebafef5d4
niolmo <71871013+niolmo@users.noreply.github.com>
2023-06-28 18:19:02 +0500

    good admin panel 2

--- NewPap/news/admin.py
+++ NewPap/news/admin.py
@@ -7,11 +7,15 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ['postCraTime', 'postType', 'postTitle', 'postText', 'postAuthor', 'postRunk', ]
     list_filter = ['postCraTime', 'postType', 'postTitle', 'postAuthor']
     search_fields = ['postCraTime', 'postTitle', 'postAuthor']
+    raw_id_fields = ['postAuthor']
     date_hierarchy = 'postCraTime'
     ordering = ['postCraTime']
 @admin.register(Author)
 class AutorAdmin(admin.ModelAdmin):
     list_display = ['author', 'authorRunk']
+    search_fields =['author']
+    raw_id_fields = ['author']
+
 
 # from .models import Author, Category, Post, Comment, PostCategory
 # admin.site.register(Author)
